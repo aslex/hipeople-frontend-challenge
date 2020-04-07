ae572797e2840590244347f187e1ad9bd7d613a9
Alexandra Servie <alexservie@MacBook-Pro-3.fritz.box>
2020-04-07 14:13:50 +0200

    some styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,49 @@
 import React, { useState, useEffect } from "react";
+// import axios from 'axios';
 
 export default function Home() {
   const [photos, setPhotos] = useState([]);
+  const [oldPhotos, setOldPhotos] = useState([]);
 
   useEffect(() => {
-    fetch("");
-  }, [photos]);
+    fetch(`https://api.unsplash.com/photos/random?count=30`, {
+      method: "GET",
+      headers: {
+        "Accept-Version": "v1",
+        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        console.log("api response:", res);
+        setPhotos(res);
+      });
+  }, []);
 
-  return <div></div>;
+  const photoStream = photos.map((image) => {
+    return (
+      <div className="photo-tile" key={image.id}>
+        <img src={image.urls.thumb} alt={`by ${image.user.name}`} />
+      </div>
+    );
+  });
+
+  const loadMore = () => {
+    setOldPhotos([...photos]);
+    setPhotos([]);
+  };
+
+  return (
+    <>
+      <div className="photo-scroll">
+        {oldPhotos.length > 0 ? oldPhotos : <></>}
+        {photoStream}
+      </div>
+      <button type="button" onClick={loadMore}>
+        Load More
+      </button>
+    </>
+  );
 }
