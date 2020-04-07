5d3b9464d7a292cc4839e9e21b037bf68bac15b6
Alexandra Servie <alexservie@MacBook-Pro-3.fritz.box>
2020-04-07 19:03:49 +0200

    smooth scroll to top

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,76 +3,62 @@ import React, { useState, useEffect } from "react";
 
 export default function Home() {
   const [photos, setPhotos] = useState([]);
-  const [oldPhotos, setOldPhotos] = useState([]);
+
   const [thumb, setThumb] = useState(true);
   const [large, setLarge] = useState(false);
+  const [loadMore, setLoadMore] = useState(false);
 
   useEffect(() => {
-    // fetch(`https://api.unsplash.com/photos/random?count=30`, {
-    //   method: "GET",
-    //   headers: {
-    //     "Accept-Version": "v1",
-    //     Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
-    //   },
-    // })
-    //   .then((res) => {
-    //     return res.json();
-    //   })
-    //   .then((res) => {
-    //     console.log("api response:", res);
-    //     setPhotos(res);
-    //   });
-  }, [photos]);
+    console.log("use effect !");
+    fetch(`https://api.unsplash.com/photos/random?count=30`, {
+      method: "GET",
+      headers: {
+        "Accept-Version": "v1",
+        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        console.log("api response:", res);
+        setPhotos([...photos, ...res]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [loadMore]);
 
-  const photoStream = photos.map((image) => {
-    if (thumb) {
-      return (
-        <div className="photo-tile" key={image.id}>
-          <img src={image.urls.thumb} alt={`by ${image.user.name}`} />
-        </div>
-      );
-    }
-    if (large) {
-      return (
-        <div className="photo-tile" key={image.id}>
-          <img src={image.urls.regular} alt={`by ${image.user.name}`} />
-        </div>
-      );
-    }
+  const photoStream = photos.map((image, index) => {
+    console.log("mapping!");
+
+    return (
+      <div className="photo-tile" key={`${image.id}${index}`}>
+        <img
+          src={image.urls.thumb}
+          alt={image.description || `by ${image.user.name}`}
+        />
+      </div>
+    );
   });
 
-  const loadMore = () => {
-    setOldPhotos([photos]);
-    setPhotos([]);
-  };
-  const handleSizeToggle = (e) => {
-    console.log("target", e.target.id);
-    if (e.target.id === "thumb") {
-      setThumb(!thumb);
-      document.querySelector("#thumb").classList.toggle("active");
-      document.querySelector("#large").classList.toggle("active");
-    } else if (e.target.id === "large") {
-      setLarge(!large);
-      document.querySelector("#thumb").classList.toggle("active");
-      document.querySelector("#large").classList.toggle("active");
-    }
+  function handleLoadMore() {
+    setLoadMore(!loadMore);
+  }
+
+  const handleScrollTop = (e) => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
     <>
       <div className="size">
-        <button id="thumb" className="active" onClick={handleSizeToggle}>
-          Small
-        </button>
-        <button id="large" onClick={handleSizeToggle}>
-          Large
+        <button className="active" onClick={handleScrollTop}>
+          scroll to top
         </button>
       </div>
-      <div className="photo-scroll">
-        {oldPhotos.length > 0 ? oldPhotos : <></>}
-        {photoStream}
-      </div>
-      <button type="button" onClick={loadMore}>
+      <div className="photo-scroll">{photoStream}</div>
+      <button type="button" onClick={handleLoadMore}>
         Load More
       </button>
     </>
